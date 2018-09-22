a94317b49b34699b60edd3e98e4059b24eb09317
Konrad Alfaro <alfaro.konrad@gmail.com>
2018-09-22 18:20:37 +0200

    Added testing frameworks and small tweaks

--- src/Infrastructure/Kernel.php
+++ src/Infrastructure/Kernel.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace TransitTracker;
+namespace TransitTracker\Infrastructure;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
@@ -40,10 +40,8 @@ final class Kernel extends BaseKernel
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
         $container->addResource(new FileResource($this->getProjectDir().'/config/bundles.php'));
-        // Feel free to remove the "container.autowiring.strict_mode" parameter
-        // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
-        $container->setParameter('container.autowiring.strict_mode', true);
         $container->setParameter('container.dumper.inline_class_loader', true);
+
         $confDir = $this->getProjectDir().'/config';
 
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
